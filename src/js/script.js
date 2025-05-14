16895f981424c7baaebe00ca62a8bbec56b5bca2
Guilherme de Paula <guilhermekuskowski0@gmail.com>
2025-05-14 14:51:45 -0300

    implementação da função de validação de usuário

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,10 @@ var totalEstoqueBaixo = 0;
 var menorSafra = 9999;
 var vinhoMaisAntigo = "";
 var continuar = "sim";
+function validarEntrada(mensagem) {
+    var entrada = prompt(mensagem);
+    while (!entrada) {
+      entrada = prompt("Campo obrigatório!\n" + mensagem);
+    }
+    return entrada;
+  }
